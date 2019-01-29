4243dbee836dd4c9aaa157b1c480c95c29a05c6a
James Shadrake <jimkarts@hotmail.co.uk>
2019-01-29 13:52:44 +0000

    app.py updated to read the database and templates folder created with tasks.html with basic for loop to display the tasks in the mongo db

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "task_manager_mini"
+app.config["MONGO_URI"] = "mongodb://task_man_admin:tma123admin.,@ds125388.mlab.com:25388/task_manager_mini"
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return "Hello World again"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html",
+    tasks=mongo.db.tasks.find())
+
+#@app.route('/')
+#def hello():
+#return "Hello World again"
     
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
