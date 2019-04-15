cdbab8275bbf0ece52a7ec01c907a67fe0407427
James Shadrake <jimkarts@hotmail.co.uk>
2019-04-15 10:11:01 +0000

    Files added prior to github push

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -1,8 +1,8 @@
-//Accordion functionality - also includes functionality for the option select
+//Accordion functionality - also includes functionality for the option select and the header navbar slide effect
 
 $(document).ready(function(){
     $('.collapsible').collapsible();
     $('select').formSelect();
+    $('.sidenav').sidenav();
 });
 
-    
